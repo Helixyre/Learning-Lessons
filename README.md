67761a1f2c84df243bc6873471c47506847b3bf9
Helixyre <leavictoria.c71@gmail.com>
2025-10-21 17:37:25 +0200

    Chp 5 LED RGB

--- README.md
+++ README.md
@@ -22,10 +22,11 @@ J’ajoute les chapitres **au fil des commits**.
 | **02b — LED** 💡 | Alterne et maintient LED allumer entre PIN2 et GPIO2 w/ BTN |
 | **03 — LED Bar** 💡 | Utilisation d'une bar LED pour faire une flowing light |
 | **04 — Breathing LED** 💡 | Analog, Digital 'breathing Light' |
+| **05 — RGB LED** 💡 | Utilisation de la LED RGB |
 
 ## ✨ Schema du chapitre
 
-![alt text](https://docs.freenove.com/projects/fnk0047/en/latest/_images/Chapter04_02.png)
+![alt text](https://docs.freenove.com/projects/fnk0047/en/latest/_images/Chapter05_03.png)
 
 > De nouveaux chapitres sont ajoutés quand j'avance sur mes mini-cours 😉
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,32 @@
 #include <Arduino.h>
 #include <ESP32PWM.h>
-#include <math.h>
 
-#define PIN_LED   2
-#define CHN       0
-#define FRQ       1000
-#define PWM_BIT   8
-#define BREATHING_SPEED 2.0
+void setColor(byte r, byte g, byte b);
+
+const byte ledPins[] = {4, 2, 15};
+const byte chns[] = {0, 1, 2};
+int red, green, blue;
 
 void setup() {
-  ledcSetup(CHN, FRQ, PWM_BIT);
-  ledcAttachPin(PIN_LED, CHN);
+  for (int i = 0; i < 3; i++) {   // setup the pwm channels, 1KHz, 8bit
+    ledcSetup(chns[i], 1000, 8);
+    ledcAttachPin(ledPins[i], chns[i]);
+  }
+  randomSeed(analogRead(0));
 }
 
 void loop() {
-  float t = millis() / 1000.0;
-  float brightness = pow(sin(t * BREATHING_SPEED), 2) * 255.0;
-  ledcWrite(CHN, (int)brightness);
-  delay(5);
+  red = random(0, 256);
+  green = random(0, 256);
+  blue = random(0, 256);
+  setColor(red, green, blue);
+  delay(200);
+}
+
+// setColor: write PWM values to the 3 channels
+void setColor(byte r, byte g, byte b) {
+  // If using a common-anode LED, invert values (255 - value)
+  ledcWrite(chns[0], 255 - r);
+  ledcWrite(chns[1], 255 - g);
+  ledcWrite(chns[2], 255 - b);
 }
